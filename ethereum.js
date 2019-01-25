07e6fda3b7252576902660196bcaf29c9577bf01
Christian Garcia <chrigarc@ciencias.unam.mx>
2019-01-24 18:53:03 -0600

    Leyendo información de ethereum

--- ethereum.js
+++ ethereum.js
@@ -1,15 +1,25 @@
 const Web3Library = require('web3');
 const HDWalletProvider = require("truffle-hdwallet-provider");
+const config = require('./config/ethereum.json');
 
-const mnemonic = "";
-const infura = "";
+const mnemonic = config.mnemonic;
+const infura = config.infura;
 
 const provider = new HDWalletProvider(mnemonic, infura);
 const web3 = new Web3Library(provider);
 
-const address = '';
+const address = config.address;
 
-web3.eth.getBlockNumber().then(result => console.log(result));
+web3.eth.getBlockNumber().then(result => console.log("blockNumber: " + result));
 
-web3.eth.getBalance(address).then(result => {console.log(result)});
+web3.eth.getBalance(address).then(result => {console.log("Balance: " + result)});
+
+
+const contractAddress = config.contract;
+
+
+const artifacts = require('./Solidity/build/contracts/SaveInfo.json');
+const contract = new web3.eth.Contract(artifacts.abi, contractAddress);
+
+contract.methods.getA().call().then(result => console.log("entero value:"  + result));
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+const Web3Library = require('web3');
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const config = require('../config/ethereum.json');
+
+const mnemonic = config.mnemonic;
+const infura = config.infura;
+const contractAddress = config.contract;
+
+const provider = new HDWalletProvider(mnemonic, infura);
+const web3 = new Web3Library(provider);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Introducción a programación al Blockchain de Ethereum' });
+});
+
+
+router.get('/entero', function(req, res, next) {
+  const artifacts = require('../Solidity/build/contracts/SaveInfo.json');
+  const contract = new web3.eth.Contract(artifacts.abi, contractAddress);
+  contract.methods.getA().call().then(result => {
+    console.log("entero value:"  + result);
+    res.render('entero', { title: 'Mostrando el valor de entero', entero: result });
+  });
 });
 
 module.exports = router;
